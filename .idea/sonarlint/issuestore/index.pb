c582a9aedd7a2effc052f85a8587d2c90ce1a1a2
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2023-02-17 12:29:00 -0300

    Projeto reta final

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,5 +7,5 @@
 settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
 @
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+B
+app/CMakeLists.txt,d\8\d88cace606db47f6a6449061e0bc053eca53f47d
--- app/src/main/java/com/example/notesapp/AddNote.kt
+++ app/src/main/java/com/example/notesapp/AddNote.kt
@@ -1,11 +1,76 @@
 package com.example.notesapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notesapp.Models.Note
+import com.example.notesapp.databinding.ActivityAddNoteBinding
+import com.example.notesapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class AddNote : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var oldNote: Note
+    private lateinit var note : Note
+    var isUpdate = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+
+            oldNote = intent.getSerializableExtra("current_note") as Note
+            binding.etTitle.setText(oldNote.title)
+            binding.etNote.setText(oldNote.note)
+            isUpdate = true
+        }catch (e : java.lang.Exception){
+
+            e.printStackTrace()
+        }
+
+        binding.imgCheck.setOnClickListener{
+
+            val title = binding.etTitle.text.toString()
+            val noteDesc = binding.etNote.text.toString()
+
+
+            if(title.isNotEmpty() || noteDesc.isNotEmpty()){
+
+                val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm a")
+
+                if(isUpdate){
+
+                    note = Note(
+                        oldNote.id,title,noteDesc,formatter.format(Date())
+                    )
+                }else {
+
+                    note = Note(
+                        null, title,noteDesc,formatter.format(Date())
+                    )
+                }
+
+                val intent = Intent()
+                intent.putExtra("note",note)
+                setResult(Activity.RESULT_OK,intent)
+                finish()
+            }else{
+
+                Toast.makeText(this,"Por favor insira a data", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+
+        binding.imgBackArrow.setOnClickListener{
+
+            onBackPressed()
+        }
+
     }
 }
